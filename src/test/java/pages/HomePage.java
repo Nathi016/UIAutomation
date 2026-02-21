ac1c2a2219e52104813978b1d741e29c645c1604
NathiMaepa <maepanathi06@gmail.com>
2026-02-21 21:53:49 +0200

    modified xpaths and maximised chrome

--- src/test/java/pages/HomePage.java
+++ src/test/java/pages/HomePage.java
@@ -6,12 +6,13 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import utility.BrowserDriver;
 
 public class HomePage extends BrowserDriver {
-    public static String loginButton_xpath = "/html/body/header/div/div/div/div[2]/div/ul/li[4]/a";
+    public static String loginButton_xpath = "//a[@href='/login']";
 
     public static void clickLoginButton() throws InterruptedException {
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
         driver.get("https://automationexercise.com/");
+        driver.manage().window().maximize();
         Thread.sleep(2000);
         driver.findElement(By.xpath(loginButton_xpath)).click();
 
--- src/test/java/pages/LogIn.java
+++ src/test/java/pages/LogIn.java
@@ -5,10 +5,10 @@ import utility.BrowserDriver;
 
 public class LogIn extends BrowserDriver {
 
-public static String emailXpath = "/html/body/section/div/div/div[1]/div/form/input[2]";
-public static String passwordxpath = "/html/body/section/div/div/div[1]/div/form/input[3]";
-public static String logInButtonXpath = "/html/body/section/div/div/div[1]/div/form/button";
-public static String errormessageXpath = "/html/body/section/div/div/div[1]/div/form/p";
+public static String emailXpath = "//input[@data-qa='login-email']";
+public static String passwordxpath = "//input[@type='password']";
+public static String logInButtonXpath = "//button[@data-qa='login-button']";
+public static String errormessageXpath = "Your email or password is incorrect!";
 public static String signupButtonXpath ="/html/body/section/div/div/div[3]/div/form/button";
 
 public static void sendkeys_email(){
@@ -22,7 +22,7 @@ public static void click_loginButton(){
     driver.findElement(By.xpath(logInButtonXpath)).click();
 }
 public static void verify_error_message(){
-    driver.findElement(By.xpath(errormessageXpath)).isDisplayed();
+    driver.findElement(By.linkText(errormessageXpath)).isDisplayed();
 }
 public static void signupButtonExist(){
     driver.findElement(By.xpath(signupButtonXpath)).isDisplayed();
